cad9a90d8ae0bff58a4d8a7f8aa9b158404f21d3
Karl Broman <kbroman@gmail.com>
2018-11-02 20:16:38 -0500

    Pull out revcomp function in separate file

---
+++ R/revcomp.R
@@ -0,0 +1,28 @@
+# function to do reverse complement
+revcomp <- function(txt, swap_snp_alleles=TRUE) {
+    comp <- function(a) {
+        b <- a
+        b[a=="A"] <- "T"
+        b[a=="C"] <- "G"
+        b[a=="G"] <- "C"
+        b[a=="T"] <- "A"
+        b[a=="a"] <- "t"
+        b[a=="c"] <- "g"
+        b[a=="g"] <- "c"
+        b[a=="t"] <- "a"
+        b[a=="["] <- "]"
+        b[a=="]"] <- "["
+        b }
+
+    result <- sapply(strsplit(txt, ""), function(a) paste(rev(comp(a)), collapse=""))
+
+    if(swap_snp_alleles) {
+        snps <- c("A/C", "A/G", "A/T",
+                  "C/A", "C/G", "C/T",
+                  "G/A", "G/C", "G/T",
+                  "T/A", "T/C", "T/G")
+        snps_rev <- snps[c(4,7,10,1,8,11,2,5,12,3,6,9)]
+        for(i in seq_along(snps)) result <- sub(snps[i], snps_rev[i], result, fixed=TRUE)
+    }
+    result
+}
